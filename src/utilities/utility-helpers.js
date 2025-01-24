2ef66a9fb68f0cda6558ebdfef8dff63751b2c56
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-01-24 18:13:37 -0500

    feat: add helper utility class

---
+++ src/utilities/utility-helpers.js
@@ -0,0 +1,18 @@
+export default class Helpers {
+    static isValueEmpty = (resourceValue) => {
+        if (resourceValue === 0 || resourceValue === undefined || resourceValue === "") {
+            return true;
+        }
+        return false;
+    }
+    static getOutOfBoundsCount = (objectList, xValue) => {
+        let baseIndexCount = 0;
+        for (let i = 0; i < objectList.length; i++) {
+            if (objectList[i].sprite.x < xValue) {
+                baseIndexCount = baseIndexCount + 1;
+            }
+        }
+        return baseIndexCount;
+    }
+}
+

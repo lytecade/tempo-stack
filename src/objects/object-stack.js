ca81c91aacda5fa61b097c71b85bfb38b2af6ea6
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-02-11 16:53:17 -0500

    fix: set collision for stack and ground

--- src/objects/object-stack.js
+++ src/objects/object-stack.js
@@ -2,5 +2,6 @@ export default class Stack {
     constructor(scene, x, y) {
         this.scene = scene;
         this.sprite = scene.physics.add.sprite(x, y, "sprite-stack", 0).setMaxVelocity(60, 60).setSize(32, 8);
+        this.spriteCollider = scene.physics.world.addCollider(this.sprite, scene.groundLayer);
     }
 }

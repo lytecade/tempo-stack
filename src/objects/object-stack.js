1ddd4142f79f5ad672d6ce60ec8f0ef32c6d00fc
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-03-15 09:10:35 -0400

    feat: add currentSize attribute to object and adjust origin point for sprite

--- src/objects/object-stack.js
+++ src/objects/object-stack.js
@@ -1,11 +1,12 @@
 import { SpeedTypes } from "../utilities/utility-helpers.js";
 export default class Stack {
-    constructor(scene, x, y, currentFrame) {
+    constructor(scene, x, y, currentFrame, currentSize) {
         this.x = x;
         this.y = y;
         this.scene = scene;
         this.currentFrame = currentFrame;
-        this.sprite = scene.physics.add.sprite(x, y, "sprite-stack", currentFrame).setMaxVelocity(60, 60).setSize(32, 8);
+        this.currentSize = currentSize;
+        this.sprite = scene.physics.add.sprite(x, y, "sprite-stack", currentFrame).setMaxVelocity(60, 60).setSize(currentSize, 8).setOrigin(0.5, 0.5);
         this.spriteCollider = scene.physics.world.addCollider(this.sprite, scene.groundLayer);
         this.movementState = true;
         this.movementRight = true;

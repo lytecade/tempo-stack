cd4d87583c7530bd32a4ecb1828443aa7a2aa048
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-05-30 17:11:23 -0400

    feat: add hud resource to scene

--- src/utilities/utility-uis.js
+++ src/utilities/utility-uis.js
@@ -10,10 +10,8 @@ export default class UIs {
     static setHudCounter = (scene) => {
         scene.hudCounters = [0, 0];
         scene.hudCounterImages = [];
-        scene.hudBar = scene.add.image(10, 9, 'sprite-hud', 16).setOrigin(1, 0).setScrollFactor(0);
-        scene.hudJumpBarCounter = 0;
         for (let i = 0; i < scene.hudCounters.length; i++) {
-            scene.hudCounterImages.push(scene.add.image(16 + (i * 4), 9, 'sprite-hud', 0).setOrigin(1, 0).setScrollFactor(0));
+            scene.hudCounterImages.push(scene.add.image(18 + (i * 4), 6, 'sprite-hud', 0).setOrigin(1, 0).setScrollFactor(0));
         }
     }
 }

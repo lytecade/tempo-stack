c3afab9a94acb3bda90fc8eacb258df61ed5f219
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-06-03 23:57:54 -0400

    feat: add update hud counter function to utilities

--- src/utilities/utility-uis.js
+++ src/utilities/utility-uis.js
@@ -7,11 +7,25 @@ export default class UIs {
             }
         });
     }
-    static setHudCounter = (scene) => {
+    static initHudCounter = (scene) => {
         scene.hudCounters = [0, 0];
         scene.hudCounterImages = [];
         for (let i = 0; i < scene.hudCounters.length; i++) {
             scene.hudCounterImages.push(scene.add.image(18 + (i * 4), 6, 'sprite-hud', 0).setOrigin(1, 0).setScrollFactor(0));
+        }
+    }
+    static updateHudCounter = (scene) => {
+        scene.hudCounters[scene.hudCounters.length - 1]++;
+        for (let i = scene.hudCounters.length - 1; i >= 0; i--) {
+            if (scene.hudCounters[i] > 9) {
+                scene.hudCounters[i] = 0;
+                if (i > 0) {
+            	scene.hudCounters[i - 1]++;
+                }
+            }
+        }
+        for (let i = 0; i < scene.hudCounters.length; i++) {
+            scene.hudCounterImages[i].setFrame(scene.hudCounters[i]);
         }
     }
 }
